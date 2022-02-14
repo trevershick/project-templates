1a6e7ea9772ce09c2ac61ab3da98f201998ec2af
Trever Shick <trevershick@gmail.com>
2022-02-13 20:57:33 -0600

    cleanup

--- realize/src/do_generate_project.c
+++ realize/src/do_generate_project.c
@@ -3,23 +3,21 @@
 // needed for getline
 #define _POSIX_C_SOURCE 200809L
 
-#include <assert.h>
-#include <ftw.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-
-#include <smatch.h>
-
 #include "ezfs.h"
 #include "ezstring.h"
 #include "realize_defs.h"
 #include "realize_io.h"
-
-char line_buffer[4096];
+#include "smatch.h"
+#include <assert.h>
+#include <ftw.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 
 // this is disappointing
 static realize_options_t *g_opts = 0;
+static smatch_node_t g_tree;
 
 static int visit_path(const char *path, const struct stat *s, int typeflag);
 static int make_dir(realize_options_t *, const char *s, const char *d,
@@ -27,22 +25,16 @@ static int make_dir(realize_options_t *, const char *s, const char *d,
 static int make_file(realize_options_t *, const char *s, const char *d,
                      bool dexists);
 
-typedef struct
-{
-    FILE *fp;
-    realize_options_t *opts;
-} smatch_data;
-
 int on_smatch_match(const match_t *m, void *d)
 {
-    smatch_data *data = d;
+    FILE *fp = d;
     if (m->t == MATCH)
     {
-        fputs(data->opts->project_name, data->fp);
+        fputs(g_opts->project_name, fp);
     }
     else
     {
-        fwrite(m->buffer, m->len, 1, data->fp);
+        fwrite(m->buffer, m->len, 1, fp);
     }
     return 0;
 }
@@ -51,6 +43,10 @@ int do_generate_project(realize_options_t *opts)
 {
     assert(!g_opts);
     g_opts = opts;
+
+    initialize_tree(&g_tree);
+    add_to_tree(&g_tree, TOKEN, true);
+
     int ret = 0;
     ret = ftw(opts->proj_tmpl_path, visit_path, FTW_MOUNT | FTW_PHYS);
     if (ret == -1)
@@ -64,23 +60,15 @@ int do_generate_project(realize_options_t *opts)
 static int visit_path(const char *path, const struct stat *s, int typeflag)
 {
     // work out the destination path
-    char destination_path[PATH_MAX];
-    memset(destination_path, 0, sizeof(destination_path));
-    memcpy(destination_path, path, strlen(path));
+    char dpath[PATH_MAX];
+    memset(dpath, 0, sizeof(dpath));
+    memcpy(dpath, path, strlen(path));
     // replace any tokens in the path and change it's relative location
-    str_replace(destination_path, sizeof(destination_path), TOKEN,
-                g_opts->project_name);
-    str_replace(destination_path, sizeof(destination_path),
-                g_opts->proj_tmpl_path, ".");
-
-    // work out a human readable source path
-    char b[PATH_MAX];
-    memset(b, 0, sizeof(b));
-    memcpy(b, path, strlen(path));
-    str_replace(b, sizeof(b), g_opts->proj_tmpls_root_path, "<root>");
+    str_replace(dpath, sizeof(dpath), TOKEN, g_opts->project_name);
+    str_replace(dpath, sizeof(dpath), g_opts->proj_tmpl_path, ".");
 
     // print out what we're doing
-    const bool destination_exists = ez_exists(destination_path);
+    const bool dexists = ez_exists(dpath);
     int ret = 0;
     char type = ' ';
     char action = ' ';
@@ -88,24 +76,32 @@ static int visit_path(const char *path, const struct stat *s, int typeflag)
     {
     case FTW_D:
         type = 'D';
-        if (!destination_exists)
+        if (!dexists)
             action = '+';
-        ret = make_dir(g_opts, path, destination_path, destination_exists);
+        ret = make_dir(g_opts, path, dpath, dexists);
         break;
     case FTW_F:
         type = 'F';
-        if (g_opts->force || !destination_exists)
+        if (g_opts->force || !dexists)
             action = '+';
-        ret = make_file(g_opts, path, destination_path, destination_exists);
+        ret = make_file(g_opts, path, dpath, dexists);
         break;
     default:
         type = '?';
         action = ' ';
     }
+
     // screen geometry calc
     int sz = (g_opts->window_size.ws_col - 10) / 2;
+
+    // work out a human readable source path
+    char b[PATH_MAX];
+    memset(b, 0, sizeof(b));
+    memcpy(b, path, strlen(path));
+    str_replace(b, sizeof(b), g_opts->proj_tmpls_root_path, "<root>");
+
     rprintf(g_opts, LOG_LEVEL_DEBUG, "%c %-*.*s -> %-*.*s  %c\n", type, sz, sz,
-            b, sz, sz, destination_path, action);
+            b, sz, sz, dpath, action);
     rprintf(g_opts, LOG_LEVEL_TRACE, "make_file returning %d\n", ret);
     return ret;
 }
@@ -115,14 +111,17 @@ static int make_dir(realize_options_t *opts, const char *src, const char *dst,
 {
     int ret = 0;
     opts->stats.template_dirs++;
-    if (!exists)
-    {
-        if (!opts->dryrun)
-            ret = mkdir(dst, DEFAULT_MODE);
-        opts->stats.dirs_added++;
-        if (ret)
-            perror(dst);
-    }
+    if (exists)
+        return 0;
+
+    if (!opts->dryrun)
+        ret = mkdir(dst, DEFAULT_MODE);
+
+    opts->stats.dirs_added++;
+
+    if (ret)
+        perror(dst);
+
     return ret;
 }
 
@@ -136,50 +135,38 @@ static int make_file(realize_options_t *opts, const char *src, const char *dst,
         return 0;
     }
 
+    off_t offset = 0;
+    char c;
+    smatch_ctx_t ctx;
+    FILE *fr = NULL;
+    FILE *fw = NULL;
     int ret = 0;
-    if (!opts->dryrun)
-    {
-        // create the file and filter its contents
-        FILE *fw = fopen(dst, "w");
-        if (fw == NULL)
-        {
-            perror(dst);
-            return -1;
-        }
 
-        FILE *fr = fopen(src, "r");
-        if (fr == NULL)
+    for (;!opts->dryrun && !fr && !fw && !ret;)
+    {
+        fr = fopen(src, "r");
+        fw = fopen(dst, "w");
+        if (fr == NULL || fw == NULL)
         {
             perror(src);
-            return -1;
+            ret = -1;
+            break;
         }
-
-        smatch_node_t tree;
-        initialize_tree(&tree);
-        add_to_tree(&tree, TOKEN, true);
-        smatch_ctx_t ctx;
-        smatch_data d;
-        d.opts = opts;
-        d.fp = fw;
-        init_smatch_ctx(&ctx, &tree, on_smatch_match, &d);
-        off_t offset = 0;
-
-        char c;
-        do {
+        init_smatch_ctx(&ctx, &g_tree, on_smatch_match, fw);
+        do
+        {
             smatch_process(&ctx, offset, c = fgetc(fr));
         } while (c != EOF);
-
-        if (fclose(fw))
-        {
-            perror("fclose");
-            return -1;
-        }
-
-        if (fclose(fr))
-        {
-            perror("fclose");
-            return -1;
-        }
+    }
+    if (fr && fclose(fr))
+    {
+        perror("fclose");
+        ret = -1;
+    }
+    if (fw && fclose(fw))
+    {
+        perror("fclose");
+        ret = -1;
     }
 
     if (exists)
